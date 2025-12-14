d4ee2dfa2abf945d03c3512c92f2a8faebbee512
Mohammed Alsaqqaf <155610155+mhsinj@users.noreply.github.com>
2025-12-14 17:02:10 +0300

    Add comments for project structure and parts
    Added structure and comments for project parts.

--- main.py
+++ main.py
@@ -1,2 +1,27 @@
 #main file of project
-print("test")
+######################
+#Part number one - banner add inputes for user
+
+
+
+
+##################################
+#part number 2 - if else of number in input
+
+
+
+
+
+
+
+
+######################################
+#Part 3 - Def Functions of every part 
+
+
+
+
+
+
+
+
